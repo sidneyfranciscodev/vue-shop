1d4022cd407e3bca5dfe46bb8b8c2c9e15eaaf30
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 18:32:14 +0200

    add global styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup></script>
+<script></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
 </template>
 
-<style scoped></style>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
