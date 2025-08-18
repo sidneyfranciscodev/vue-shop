cac0378cd79815c3745f571e3dd105789f0a644a
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 19:09:45 +0200

    add routes for ProductList

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import ProductsList from '../pages/ProductsList.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', redirect: '/products' },
+    { path: '/products', component: ProductsList },
+  ],
 })
 
 export default router
