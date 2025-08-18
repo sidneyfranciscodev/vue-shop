d34c570f3fb42ad134f57f899e7f5bf9a7500dc6
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 20:48:01 +0200

    change ProductItem style lang to SCSS

--- src/components/products/ProductItem.vue
+++ src/components/products/ProductItem.vue
@@ -27,52 +27,54 @@ export default {
 };
 </script>
 
-<style scoped>
-li {
+<style lang="scss" scoped>
+.product {
   margin: 1.5rem auto;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
 
-.product__data {
-  display: flex;
-}
+  .product__data {
+    display: flex;
 
-.product__image {
-  margin-right: 1rem;
-}
+    .product__image {
+      margin-right: 1rem;
 
-.product__image img {
-  height: 10rem;
-  width: 10rem;
-  object-fit: cover;
-}
+      img {
+        height: 10rem;
+        width: 10rem;
+        object-fit: cover;
+      }
+    }
 
-.product__text h3 {
-  margin: 0 0 0.5rem 0;
-}
+    .product__text {
+      h3 {
+        margin: 0 0 0.5rem 0;
+      }
 
-.product__text h4 {
-  margin: 0;
-}
+      h4 {
+        margin: 0;
+      }
+    }
+  }
 
-.product__actions {
-  text-align: center;
-}
+  .product__actions {
+    text-align: center;
 
-button {
-  font: inherit;
-  cursor: pointer;
-  background-color: #45006d;
-  color: white;
-  border: 1px solid #45006d;
-  padding: 0.5rem 1.5rem;
-  border-radius: 30px;
-}
+    button {
+      font: inherit;
+      cursor: pointer;
+      background-color: #45006d;
+      color: white;
+      border: 1px solid #45006d;
+      padding: 0.5rem 1.5rem;
+      border-radius: 30px;
 
-button:hover,
-button:active {
-  background-color: #760ab4;
-  border-color: #760ab4;
+      &:hover,
+      &:active {
+        background-color: #760ab4;
+        border-color: #760ab4;
+      }
+    }
+  }
 }
 </style>
