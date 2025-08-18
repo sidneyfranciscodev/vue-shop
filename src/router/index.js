34ec7671cc094562acbc940ac4d810e2203c9af8
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 20:57:33 +0200

    add route for UserCart

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import ProductsList from '../pages/ProductsList.vue';
+import UserCart from '../pages/UserCart.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/products' },
     { path: '/products', component: ProductsList },
+    { path: '/cart', component: UserCart },
   ],
 })
 
