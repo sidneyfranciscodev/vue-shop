56017d585262056673d5a71122e9a918e3676b20
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 19:07:26 +0200

    create ProductList page

---
+++ src/pages/ProductsList.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <ul>
+      <product-item
+        v-for="prod in products"
+        :key="prod.id"
+        :id="prod.id"
+        :title="prod.title"
+        :image="prod.image"
+        :description="prod.description"
+        :price="prod.price"
+      ></product-item>
+    </ul>
+  </section>
+</template>
+
+<script>
+import ProductItem from '../components/products/ProductItem.vue';
+
+export default {
+  data(){
+    return {
+      products: [
+        {
+          id: 1,
+          title: 'product-1',
+          image: 'image',
+          description: 'description',
+          price: 0.99
+        },
+        {
+          id: 2,
+          title: 'product-2',
+          image: 'image',
+          description: 'description',
+          price: 2.99
+        },
+      ]
+    }
+  },
+  components: {
+    ProductItem,
+  },
+};
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 40rem;
+  }
+</style>
