c1f78147f758378ca64b4cc9052090611abd05fb
Sidney <sidneyfranciscodev@gmail.com>
2025-08-18 18:41:56 +0200

    create TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script></script>
-
 <template>
-
+  <the-header></the-header>
 </template>
 
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+}
+</script>
 
 <style>
 * {
---
+++ src/components/nav/TheHeader.vue
@@ -0,0 +1,92 @@
+<template>
+  <header>
+    <h1>
+      <router-link to="/">VueShop</router-link>
+    </h1>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/products">Products</router-link>
+        </li>
+        <li>
+          <router-link to="/cart">Cart</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <button v-if="!isLoggedIn" @click="login">Login</button>
+      <button v-if="isLoggedIn" @click="logout">Logout</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      isLoggedIn: false,
+    }
+  },
+  methods: {
+    login() {
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 5rem;
+  background-color: white;
+  margin: 0 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-self: center;
+    align-items: center;
+
+    li {
+      margin: 0 1rem;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-weight: bold;
+        border-bottom: 2px solid transparent;
+        padding-bottom: 0.25rem;
+
+        &:hover,
+        &:active {
+          color: #45006d;
+          border-color: #45006d;
+        }
+      }
+    }
+  }
+
+  button {
+    font: inherit;
+    cursor: pointer;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #45006d;
+    background-color: transparent;
+    color: #45006d;
+    border-radius: 30px;
+
+    &:hover,
+    &:active {
+      background-color: #f0d5ff;
+    }
+  }
+}
+</style>
